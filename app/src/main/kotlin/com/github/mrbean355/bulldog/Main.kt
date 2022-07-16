9fb146ba14cd34d6fa738fb805bc52fb1d0633ed
Mike Johnston <mrbean355@gmail.com>
2022-07-16 17:43:16 +0200

    Implement main screen structure

--- app/src/main/kotlin/com/github/mrbean355/bulldog/Main.kt
+++ app/src/main/kotlin/com/github/mrbean355/bulldog/Main.kt
@@ -1,31 +1,21 @@
 package com.github.mrbean355.bulldog
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.application
 import com.github.mrbean355.bulldog.components.AppWindow
 import com.github.mrbean355.bulldog.localization.getString
-
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
 
 fun main() = application {
     AppWindow(
@@ -33,6 +23,43 @@ fun main() = application {
         escapeClosesWindow = false,
         onCloseRequest = ::exitApplication
     ) {
-        App()
+        var activeTab by remember { mutableStateOf(MainTab.Home) }
+
+        Scaffold(
+            topBar = {
+                TabRow(
+                    selectedTabIndex = activeTab.ordinal,
+                    modifier = Modifier.height(48.dp)
+                ) {
+                    MainTab.values().forEach {
+                        Tab(
+                            selected = activeTab == it,
+                            onClick = { activeTab = it },
+                            content = { Text(getString(it.label)) }
+                        )
+                    }
+                }
+            }
+        ) {
+            activeTab.content()
+        }
     }
+}
+
+private enum class MainTab(
+    val label: String,
+    val content: @Composable () -> Unit
+) {
+    Home(label = "home.tab.home", content = { Placeholder() }),
+    Sounds(label = "home.tab.sounds", content = { Placeholder() }),
+    DiscordBot(label = "home.tab.discord", content = { Placeholder() }),
+    Settings(label = "home.tab.settings", content = { Placeholder() }),
+}
+
+@Composable
+private fun Placeholder() {
+    Text(
+        text = "This is not the tab you're looking for.",
+        modifier = Modifier.padding(16.dp)
+    )
 }
