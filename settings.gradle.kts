7fea4466168686b164ee0336c75ddc586bd3c58b
Mike Johnston <mrbean355@gmail.com>
2022-08-23 22:08:06 +0200

    Upgrade Kotlin, Compose & Serialization

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,9 @@ rootProject.name = "admiralbulldog-app"
 
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.7.0"
-        kotlin("plugin.serialization") version "1.7.0"
-        id("org.jetbrains.compose") version "1.2.0-alpha01-dev755"
+        kotlin("jvm") version "1.7.10"
+        kotlin("plugin.serialization") version "1.7.10"
+        id("org.jetbrains.compose") version "1.2.0-alpha01-dev764"
     }
 }
 
